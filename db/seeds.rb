2820de5809fa39353e55c7e7aa37fb0027f3fbdb
santolp <wipidev@gmail.com>
2025-01-27 23:45:53 -0300

    Clean code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   [Action, Comedy, Drama, Horror].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-
 #Importamos la gema faker para generar datos aleatorios
 require 'faker'
 
@@ -82,19 +71,3 @@ end
 end
 
 
-##
-#Primero, destruye todas las películas para tener un estado limpio y agrega tres películas pasando una matriz al método de creación. Esas prácticas instrucciones Ruby X.ago para definir fechas las proporcionan las extensiones principales de Active Support.
-
-#Al final, hay algunos comentarios sobre la cantidad total de películas creadas. ¡Ejecutémoslo!
-# bin/rails db:seed
-# "Created 3 productos"
-
-
-# Puedes ejecutar este comando tantas veces como necesites, ya que los registros existentes se eliminan gracias a la primera línea que contiene la sentencia destroy.
-
-# Para comprobarlos, puedes utilizar rails runner:
-
-# bin/rails runner "p Producto.pluck :name"
-# # ["Tee", "Shoes", "Joggins"]
-
-##
